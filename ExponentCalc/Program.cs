b4a1328f5689fc9d5ecae8f9679de4d9604a7cee
russtack <stirlingest@gmail.com>
2023-04-08 12:43:36 -0400

    calculator

---
+++ ExponentCalc/Program.cs
@@ -0,0 +1,18 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine(GetPow(5, 4));
+       
+    }
+     static int GetPow(int baseNum, int powNum)
+    {
+        int result = 1;
+        for (int i = 0; i < powNum; i++)
+        {
+            result *= baseNum;
+
+        }
+        return result;
+    }
+}
